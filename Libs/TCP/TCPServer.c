fcf059d8bbbe4dd3f6ae2cfefcf7a7f417239fae
Kevin Lundell <nocktok123@gmail.com>
2022-10-16 16:13:16 +0200

    Fixed TCPServer

--- Libs/TCP/TCPServer.c
+++ Libs/TCP/TCPServer.c
@@ -84,8 +84,8 @@ int TCPServer_Listen(TCPServer* _TCPServer, const char* _IP, UInt16 _Port)
 	printf("TCPServer_Listen(%u): %s\n\r", _Port, _IP != NULL ? _IP : "localhost");
 	memset(&_TCPServer->m_ServerAddr, 0, sizeof(_TCPServer->m_ServerAddr));
 	_TCPServer->m_ServerAddr.sin_family = AF_INET;
-	_TCPServer->m_ServerAddr.sin_port = _IP != NULL ? htons(_Port) : htonl(INADDR_ANY);
-	_TCPServer->m_ServerAddr.sin_addr.s_addr = inet_addr(_IP);
+	_TCPServer->m_ServerAddr.sin_port = htons(_Port);
+	_TCPServer->m_ServerAddr.sin_addr.s_addr = strcmp("127.0.0.1", _IP) == 0 || strcmp("localhost", _IP) == 0 ? htonl(INADDR_ANY) : inet_addr(_IP);
 
 	int success = bind(_TCPServer->m_Socket, (struct sockaddr*)&_TCPServer->m_ServerAddr, sizeof(_TCPServer->m_ServerAddr));
 	if(success != 0)
