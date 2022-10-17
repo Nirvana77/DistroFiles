ae3ee45e9b122a912735cfbde9cd8d82532edecc
Kevin Lundell <nocktok123@gmail.com>
2022-10-17 05:20:52 +0200

    Added networkLayer files

---
+++ Libs/Communication/NetworkLayer.c
@@ -0,0 +1,67 @@
+int NetworkLayer_InitializePtr(NetworkLayer** _NetworkLayerPtr)
+{
+    NetworkLayer* _NetworkLayer = (NetworkLayer*)Allocator_Malloc(sizeof(NetworkLayer));
+    if(_NetworkLayer == NULL)
+        return -1;
+    
+    int success = NetworkLayer_Initialize(_NetworkLayer);
+    if(success != 0)
+    {
+        Allocator_Free(_NetworkLayer);
+        return success;
+    }
+    
+    _NetworkLayer->m_Allocated = True;
+    
+    *(_NetworkLayerPtr) = _NetworkLayer;
+    return 0;
+}
+
+int NetworkLayer_Initialize(NetworkLayer* _NetworkLayer)
+{
+    _NetworkLayer->m_Allocated = False;
+    
+    return 0;
+}
+
+int NetworkLayer_SendPayload(void* _Context, Payload* _Paylode)
+{
+	NetworkLayer* _NetworkLayer = (NetworkLayer*) _Context;
+
+	printf("NetworkLayer_SendPayload\n\r");
+
+    UInt8 type = __UINT8_MAX__;
+    
+    Memory_ParseUInt8();
+
+	_NetworkLayer->m_FuncOut.m_Send(_NetworkLayer->m_FuncOut.m_Context, _Paylode);
+
+
+	return 0;
+}
+
+int NetworkLayer_ReveicePayload(void* _Context, Payload* _Paylode)
+{
+	NetworkLayer* _NetworkLayer = (NetworkLayer*) _Context;
+
+	printf("NetworkLayer_ReveicePayload\n\r");
+	
+	Memory_ParseUInt16(_Paylode->m_Data.BUFFER, &_Paylode->m_Size);
+
+	int reviced = _NetworkLayer->m_FuncOut.m_Receive(_NetworkLayer->m_FuncOut.m_Context, _Paylode);
+
+	return 0;
+}
+
+
+
+void NetworkLayer_Dispose(NetworkLayer* _NetworkLayer)
+{
+    
+
+    if(_NetworkLayer->m_Allocated == True)
+        Allocator_Free(_NetworkLayer);
+    else
+        memset(_NetworkLayer, 0, sizeof(NetworkLayer));
+
+}
---
+++ Libs/Communication/NetworkLayer.h
@@ -0,0 +1,26 @@
+#ifndef NetworkLayer_h__
+#define NetworkLayer_h__
+
+struct T_NetworkLayer;
+typedef struct T_NetworkLayer NetworkLayer;
+
+#include "Payload.h"
+#include "../Memory.h"
+
+struct T_NetworkLayer
+{
+    Bool m_Allocated;
+    
+	Payload_FuncOut m_FuncOut;
+
+};
+
+int NetworkLayer_InitializePtr(NetworkLayer** _NetworkLayerPtr);
+int NetworkLayer_Initialize(NetworkLayer* _NetworkLayer);
+
+
+int NetworkLayer_SendPayload(void* _Context, Payload* _Paylode);
+int NetworkLayer_ReveicePayload(void* _Context, Payload* _Paylode);
+
+void NetworkLayer_Dispose(NetworkLayer* _NetworkLayer);
+#endif // NetworkLayer_h__
