b8c4f9102647e1d458359d2bc8cdc617f7e60f67
Kevin Lundell <nocktok123@gmail.com>
2022-11-05 17:10:19 +0900

    Removed main function in client.py

--- Libs/GUI/client.py
+++ Libs/GUI/client.py
@@ -34,36 +34,3 @@ class Client:
 				print("message: ", message)
 				return (method, message)
 		return ("", [])
-
-def client_program():
-		client_socket = connect(socket.gethostname(), 8021)
-		data = ""
-		method = ""
-		message = ""
-		while data != "q":
-			os.system('clr')
-			print("1) Upload file")
-			print("2) Remove file")
-			print("3) Rename file")
-
-			data = input('-> ')
-
-			if(data == "1"):
-				send_file(client_socket, "setting.json")
-
-			elif(data != "q"):
-				data = ""
-				
-
-			if(data == "s"):
-				arr = messag_builder("1", "", method, message)
-				print(arr)
-				
-				client_socket.send(bytearray(arr))
-		
-
-		client_socket.close()  # close the connection
-
-
-if __name__ == '__main__':
-	client_program()
